8cfce525b768514325a7462d6898e5eac9967a6a
Elenore Bastian <ebastian@pivotal.io>
2020-07-28 12:03:33 -0400

    test deploy script

--- pipeline/deploy.sh
+++ pipeline/deploy.sh
@@ -7,27 +7,10 @@ echo $KUBECONFIG > $HOME/.kube/config
 
 image=$(cat build-service-sample-app/image)
 
-echo $image
-
-cat << EOF
-apiVersion: apps/v1
-kind: Deployment
-metadata:
-  name: build-service-sample-app
-  labels:
-    app: build-service-sample-app
-spec:
-  selector:
-    matchLabels:
-      app: build-service-sample-app
-  template:
-    metadata:
-      labels:
-        app: build-service-sample-app
-    spec:
-      containers:
-      - name: build-service-sample-app
-        image: $image
-        ports:
-        - containerPort: 80
+cat <<\EOF > values.yml
+#@data/values
+---
+image:
 EOF
+
+ytt -v image="${image}" -f values.yml -f source-code/resources/deployment.yml | kubectl apply -f
